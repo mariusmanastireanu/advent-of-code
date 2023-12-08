45b579d6a928707c9207810ff7ec7702e54a35fb
Marius Manastireanu <marius.manastireanu@gmail.com>
2023-12-08 12:30:43 +0200

    Reformat code

--- src/main/java/org/advent/helper/MathUtil.java
+++ src/main/java/org/advent/helper/MathUtil.java
@@ -5,15 +5,14 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class MathUtil {
 
-    public static long lcm(long a, long b)
-    {
+    public static long lcm(long a, long b) {
         return (a / gcd(a, b)) * b;
     }
 
-    public static long gcd(long a, long b)
-    {
-        if (a == 0)
+    public static long gcd(long a, long b) {
+        if (a == 0) {
             return b;
+        }
         return gcd(b % a, a);
     }
 
